8c0d5505a2bddf7937a02b33396a5040a1a9c845
iitiansuyash <suyashsuryanshi11111@gmail.com>
2022-11-12 14:26:29 +0530

    Ready to deploy

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.use((req, res) => {
     res.json("404");
 })
 
-app.listen("3000", () => {
-    console.log('Listening to the port 3000!');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port 3000');
 })
